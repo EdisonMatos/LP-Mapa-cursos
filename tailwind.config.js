b3efbdf27a1f513114990f15344da93e09323454
Gabriel <gabrielsouza65115@gmail.com>
2025-03-04 11:44:47 -0300

    fix: Deixa as labels do site na cor branca

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export const theme = {
       buttonColor: "#DBB14C",
       lighter: "#FFFFFF",
       darker: "#000000",
-      labelsIcons: "#000000",
+      labelsIcons: "#FFFFFF",
     },
     boxShadow: {
       "custom-opacity": "-20px 20px 0px", // 30% de opacidade
