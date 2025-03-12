a0f7c64849de9292d271ec4c8fc597abd0bbc7d5
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-12 17:34:44 -0300

    fix: Remove o paddingTop da Sessão de Cursos Anteriores

--- src/components/sections/CursosAnteriores.jsx
+++ src/components/sections/CursosAnteriores.jsx
@@ -11,7 +11,7 @@ import imgCursoAnterior3 from "../../assets/imgs/hero/cursoAnteriorHero3.webp";
 export default function CursosMinistrados() {
   return (
     <div>
-      <SectionArea paddingbot={false} className="bg-bgSectionDark">
+      <SectionArea paddingtop={false} className="bg-bgSectionDark">
         <SectionWrapper>
           <SectionHeader
             className="text-center"
