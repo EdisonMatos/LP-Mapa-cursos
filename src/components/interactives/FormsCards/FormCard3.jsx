109963b479eecbfb164968c16c40544a9b1f8e5c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-03-28 21:13:18 -0300

    fix: Numero form3

--- src/components/interactives/FormsCards/FormCard3.jsx
+++ src/components/interactives/FormsCards/FormCard3.jsx
@@ -19,7 +19,6 @@ const FormCard3 = () => {
   const [email, setEmail] = useState("");
   const [empresa, setEmpresa] = useState("");
   const [cargo, setCargo] = useState("");
-  const [institution, setInstitution] = useState("");
 
   // const [message, setMessage] = useState("");
   const [errors, setErrors] = useState({});
@@ -73,13 +72,10 @@ const FormCard3 = () => {
       validationErrors.email = "E-mail inválido.";
     }
 
-    if (!institution) {
-      validationErrors.institution = "O campo Instituição é obrigatório.";
-    } else !validateInstitution(institution);
-
+   
     if (!cpf) {
       validationErrors.cpf = "O campo Cpf é obrigatório.";
-    } else !validateInstitution(institution);
+    } else !validateCpf(cpf);
 
     if (!cargo) {
       validationErrors.cargo = "O campo Função é obrigatório.";
@@ -92,11 +88,8 @@ const FormCard3 = () => {
 
     if (!uf) {
       validationErrors.uf = "O campo Uf é obrigatório.";
-    } else !validateInstitution(uf);
+    } else !validateUf(uf);
 
-    // if (!validateMessage(message)) {
-    //   validationErrors.message = "O campo mensagem é obrigatório.";
-    // }
 
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
@@ -105,7 +98,7 @@ const FormCard3 = () => {
     }
 
     // Aqui o número do WhatsApp precisa estar no formato correto
-    const whatsappNumber = "5599984234461"; // Certifique-se de que este número está correto com o código do país
+    const whatsappNumber = "45991290837"; // Envio pro wpp
     const formattedPhone = phone.replace(/\D/g, ""); // Remover caracteres não numéricos
 
     const whatsappMessage = `Olá! Meu nome é ${name}.%0A
@@ -113,8 +106,8 @@ const FormCard3 = () => {
     Telefone: ${formattedPhone}.%0A
     Município - UF: ${uf}.%0A
     E-mail: ${email}.%0A
-    Profission: ${profission}.%0A
-    Instituição: ${institution}`;
+    Profission: ${cargo}.%0A
+    Instituição: ${empresa}`;
 
     const whatsappURL = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
 
@@ -151,11 +144,7 @@ const FormCard3 = () => {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailPattern.test(email.trim());
   };
-  const validateInstitution = (institution) => {
-    const institutionPattern = /^[a-zA-ZÀ-ÿ\s]{5,}$/; // Permite pelo menos 5 caracteres (letras e espaços)
-    return institutionPattern.test(institution.trim());
-  };
-
+ 
   const validateCargo = (cargo) => {
     const cargoPattern = /^[a-zA-ZÀ-ÿ\s]{5,}$/; // Permite pelo menos 5 caracteres (letras e espaços)
     return cargoPattern.test(cargo.trim());
@@ -166,11 +155,15 @@ const FormCard3 = () => {
     return empresaPattern.test(empresa.trim());
   };
 
+  const validateCpf = (cpf) => {
+    const cpfPattern = /^[a-zA-ZÀ-ÿ\s]{5,}$/; // Permite pelo menos 5 caracteres (letras e espaços)
+    return cpfPattern.test(cpf.trim());
+  };
+
   const validateUf = (uf) => {
     return uf.trim().length >= 5; // Requer ao menos 5 caracteres para Cidade e Estado
   };
 
-  // const validateMessage = (message) => !!message;
 
   const formatPhoneNumber = (phoneNumber) => {
     let cleaned = phoneNumber.replace(/\D/g, ""); // Remove tudo que não for número
@@ -194,7 +187,6 @@ const FormCard3 = () => {
   return (
     <div className=" bg-[#0E2B40] p-6 rounded-[10px] w-full h-auto">
       <div className="w-full text-paragraph3 phone3:text-paragraph4 ">
-        {/* <h1 className="w-full mb-2 font-medium text-white">Fale conosco</h1> */}
         {/* Nome */}
         <div className="mb-6">
           <div className="flex mb-2 text-gray-500 tablet1:mb-0">
