74f51a5e804e947e233c2126a77da7bc4714a370
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-19 15:02:43 -0300

    fix: Seção Programação do Curso da Lpv

--- src/components/sections/lp01/FeaturesLP.jsx
+++ src/components/sections/lp01/FeaturesLP.jsx
@@ -3,6 +3,7 @@ import SectionArea from "../../sectionElements/SectionArea";
 import SectionHeader from "../../sectionElements/SectionHeader";
 import SectionWrapper from "../../sectionElements/SectionWrapper";
 import FeatureCardLP from "./FeatureCardLP";
+import { cursosDataBase } from "../../../content/contentCursos";
 
 export default function FeaturesLP({ className }) {
   return (
@@ -17,27 +18,35 @@ export default function FeaturesLP({ className }) {
           />
           <div className="w-full flex flex-wrap justify-evenly gap-y-[32px]">
             <FeatureCardLP
-              icon={contentLp01.features.cards.card1.icon}
-              title={contentLp01.features.cards.card1.title}
-              description={contentLp01.features.cards.card1.description}
+              icon={cursosDataBase.landingPage.programacao.card1.icon}
+              title={cursosDataBase.landingPage.programacao.card1.titulo}
+              description={
+                cursosDataBase.landingPage.programacao.card1.subtitulo
+              }
               className="w-full tablet1:w-[60%]"
             />
             <FeatureCardLP
-              icon={contentLp01.features.cards.card2.icon}
-              title={contentLp01.features.cards.card2.title}
-              description={contentLp01.features.cards.card2.description}
+              icon={cursosDataBase.landingPage.programacao.card2.icon}
+              title={cursosDataBase.landingPage.programacao.card2.titulo}
+              description={
+                cursosDataBase.landingPage.programacao.card2.subtitulo
+              }
               className="w-full tablet1:w-[60%]"
             />
             <FeatureCardLP
-              icon={contentLp01.features.cards.card3.icon}
-              title={contentLp01.features.cards.card3.title}
-              description={contentLp01.features.cards.card3.description}
+              icon={cursosDataBase.landingPage.programacao.card3.icon}
+              title={cursosDataBase.landingPage.programacao.card3.titulo}
+              description={
+                cursosDataBase.landingPage.programacao.card3.subtitulo
+              }
               className="w-full tablet1:w-[60%]"
             />
             <FeatureCardLP
-              icon={contentLp01.features.cards.card4.icon}
-              title={contentLp01.features.cards.card4.title}
-              description={contentLp01.features.cards.card4.description}
+              icon={cursosDataBase.landingPage.programacao.card4.icon}
+              title={cursosDataBase.landingPage.programacao.card4.titulo}
+              description={
+                cursosDataBase.landingPage.programacao.card4.subtitulo
+              }
               className="w-full tablet1:w-[60%]"
             />
           </div>
--- src/content/contentCursos.jsx
+++ src/content/contentCursos.jsx
@@ -183,117 +183,146 @@ const cursosDataBase = {
         },
       },
     },
-    curso4: {
-      id: "004",
-      nome: "A_Definir",
-      data: "A_Definir",
-      hora: "A_Definir",
-      titulo: "A_Definir",
-      subtitulo: "A_Definir",
-      status: "A_Definir", //realizado ou futuro
-      rota: "A_Definir", // (/rota)
-      imgs: {
-        imgCard: { imgCurso3 }, //img do card
-        imgHero: bgImg,
-      },
-      labelButton: "Em breve!",
-      landingPage: {
-        programacao: {
-          icon: (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-id-card"
-            >
-              <path d="M16 10h2" />
-              <path d="M16 14h2" />
-              <path d="M6.17 15a3 3 0 0 1 5.66 0" />
-              <circle cx="9" cy="11" r="2" />
-              <rect x="2" y="5" width="20" height="14" rx="2" />
-            </svg>
-          ),
-          titulo: (
-            <h1>
-              <span className="opacity-100 text-primary text-paragraph5">
-                Dia 22/04 – Recepção e Credenciamento
-              </span>{" "}
-            </h1>
-          ),
-          subtitulo: (
-            <p>
-              <strong>15:00 às 17:00</strong>
-              <br />- Credenciamento e confirmação de inscrições <br /> -
-              Networking inicial com participantes
-            </p>
-          ),
-        },
-        inscricao: {
-          icon: (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-plane"
-            >
-              <path d="M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z" />
-            </svg>
-          ),
-          title: "Acadêmicos de universidades e faculdades",
-          valor: "R$200,00",
-          buttonLabel: "Inscrever-se",
-        },
-      },
-    },
   },
   landingPage: {
     programacao: {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-id-card"
-        >
-          <path d="M16 10h2" />
-          <path d="M16 14h2" />
-          <path d="M6.17 15a3 3 0 0 1 5.66 0" />
-          <circle cx="9" cy="11" r="2" />
-          <rect x="2" y="5" width="20" height="14" rx="2" />
-        </svg>
-      ),
-      titulo: (
-        <h1>
-          <span className="opacity-100 text-primary text-paragraph5">
-            Dia 22/04 – Recepção e Credenciamento
-          </span>{" "}
-        </h1>
-      ),
-      subtitulo: (
-        <p>
-          <strong>15:00 às 17:00</strong>
-          <br />- Credenciamento e confirmação de inscrições <br /> - Networking
-          inicial com participantes
-        </p>
-      ),
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-id-card"
+          >
+            <path d="M16 10h2" />
+            <path d="M16 14h2" />
+            <path d="M6.17 15a3 3 0 0 1 5.66 0" />
+            <circle cx="9" cy="11" r="2" />
+            <rect x="2" y="5" width="20" height="14" rx="2" />
+          </svg>
+        ),
+        titulo: "Dia 22/04 – Recepção e Credenciamento",
+        subtitulo: (
+          <p>
+            <strong>15:00 às 17:00</strong>
+            <br />- Credenciamento e confirmação de inscrições <br /> -
+            Networking inicial com participantes
+          </p>
+        ),
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-notebook-pen"
+          >
+            <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4" />
+            <path d="M2 6h4" />
+            <path d="M2 10h4" />
+            <path d="M2 14h4" />
+            <path d="M2 18h4" />
+            <path d="M21.378 5.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
+          </svg>
+        ),
+        titulo: "Dia 23/04 – Improbidade e Controle Fiscal",
+        subtitulo: (
+          <p>
+            <strong>09:00 às 12:00 – Abertura</strong> <br /> - Entrega de
+            material <br /> - Apresentação dos temas
+            <br /> <br />
+            <strong>14:00 às 17:00 – Aula</strong>
+            <br />- Nova Lei de Improbidade Administrativa <br /> - Atos de
+            improbidade e controle fiscal <br /> - Orientações jurídicas para o
+            controle de contas e atos
+            <br />
+            <br /> <strong>15:30 - Coffee Break</strong>
+          </p>
+        ),
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-notebook-pen"
+          >
+            <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4" />
+            <path d="M2 6h4" />
+            <path d="M2 10h4" />
+            <path d="M2 14h4" />
+            <path d="M2 18h4" />
+            <path d="M21.378 5.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
+          </svg>
+        ),
+        titulo: " Dia 24/04 – Procedimentos e Licitações",
+        subtitulo: (
+          <p>
+            <strong>09:00 às 12:00 – Aula</strong>
+            <br /> - Diferenças na prestação de contas
+            <br />
+            - Transparência em procedimentos administrativos
+            <br /> - Prevenção ao abuso de poder e fraudes
+            <br />
+            <br /> <strong>14:00 às 17:00 – Aula</strong>
+            <br />- Limites da dispensa de licitação
+            <br />- Métodos de contratação preventiva
+            <br />- Exemplos práticos para evitar erros
+            <br />
+            <br /> <strong>15:30 - Coffee Break</strong>
+          </p>
+        ),
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-badge"
+          >
+            <path d="M12 22h6a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v3" />
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="M5 17a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+            <path d="M7 16.5 8 22l-3-1-3 1 1-5.5" />
+          </svg>
+        ),
+        titulo: "Dia 25/04 – Encerramento e Certificação",
+        subtitulo: (
+          <p>
+            <strong>09:00 às 12:00</strong>
+            <br />
+            <br /> - Debate e esclarecimento de dúvidas
+            <br /> - Entrega de certificados
+          </p>
+        ),
+      },
     },
     inscricao: {
       icon: (
